cea21772444c826778229dd6c4137262230ec7bf
rafiody16 <rafiody16@gmail.com>
2023-10-30 10:54:52 +0700

    Implementasi No 10

--- Bioskop22.java
+++ Bioskop22.java
@@ -16,10 +16,16 @@ public class Bioskop22 {
         System.out.printf("%s \t %s\n", penonton[2][0], penonton [2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton [3][1]);
 
-        System.out.println(penonton.length);
-        for (int i = 0; i < penonton.length; i++)
+        System.out.println("Penonton pada baris ke-3: ");
+        for (int i = 0; i < penonton[2].length; i++)
         {
-            System.out.println("Panjang baris ke-"+ (i+1) +": " + penonton[i].length);
+            System.out.println(penonton[2][i]);
+        }
+
+        System.out.println(penonton.length);
+        for (String[] barisPenonton : penonton)
+        {
+            System.out.println("Panjang baris: "+ barisPenonton.length);
         }
     }
 }
